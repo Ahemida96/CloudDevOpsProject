e023717ac371924b9a47c4e51466e66243afe0fa
Meda <ahmedhemida121@gmail.com>
2025-04-08 00:53:33 +0200

    Fix undeclared vars issue

--- jenkins/vars/ManifestUpdate.groovy
+++ jenkins/vars/ManifestUpdate.groovy
@@ -1,9 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String image_name, String K8S_CRED_ID, String minikube_server, String deployment_file) {
-    sh "sed -i 's|${username}/${image_name}:.*|${username}/${image_name}:${BUILD_ID}|g' ${deployment_file}"
-}
-
 def call (Map config) {
     def image_name = config.image_name
     // def github-credentials = config.github_credentials
@@ -11,19 +7,20 @@ def call (Map config) {
     def manifestDir = config.manifestDir
     def gitUsername = config.username
     def gitEmail = config.email
+    def dockerhubUsername = config.dockerhub_username
     // def manifest_repo = config.manifest_repo
 
     if (!fileExists("${manifestDir}/${manifestFile}")) {
         error("Manifest file ${manifestDir}/${manifestFile} not found!")
     }
     dir("${manifestDir}") {
-        sh "sed -i 's|${username}/${image_name}:.*|${username}/${image_name}:${BUILD_ID}|g' ${manifestFile}"
+        sh "sed -i 's|${dockerhubUsername}/${image_name}:.*|${dockerhubUsername}/${image_name}:${BUILD_ID}|g' ${manifestFile}"
     }
 
     // git branch: 'main', url: "${manifest_repo}", credentialsId: "${github-credentials}"
     sh "git config user.name ${gitUsername}"
     sh "git config user.email ${gitEmail}"
     sh "git add ${manifestFile}"
-    sh "git commit -m 'Updated image to ${username}/${image_name}:${BUILD_ID}'"
+    sh "git commit -m 'Updated image to ${dockerhubUsername}/${image_name}:${BUILD_ID}'"
     sh "git push origin master"
 }
