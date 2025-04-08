98afe4d00eb7c23d0092df9f86e286b5a2365bf6
Meda <ahmedhemida121@gmail.com>
2025-04-08 02:04:17 +0200

    Update jenkins file

--- jenkins/vars/Docker.groovy
+++ jenkins/vars/Docker.groovy
@@ -3,7 +3,7 @@
 def call(String image_name, String dockerHubCredentialsID) {
     withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        sh 'docker build ${USERNAME}/${image_name}:${BUILD_ID} .'
+        sh 'docker build -t ${USERNAME}/${image_name}:${BUILD_ID} .'
         sh 'docker push ${USERNAME}/${image_name}:${BUILD_ID}'
         sh 'docker rmi ${USERNAME}/${image_name}:${BUILD_ID}'
     }
